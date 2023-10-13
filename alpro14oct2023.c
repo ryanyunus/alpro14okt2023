c4be2d4046d3be067fe5c35de988306abd4dc5ab
ryanyunus <ryandinus@gmail.com>
2023-10-14 03:52:52 +0700

    tambahan

--- alpro14oct2023.c
+++ alpro14oct2023.c
@@ -3,47 +3,67 @@
 
 int main() {
    
+//ini rumus persegi panjang 
+int panjang;
+int lebar;
+int luas;
+
+printf("masukan panjang persegi panjang:");
+scanf("%d",&panjang);
+printf("masukan lebar persegi panjang:");
+scanf("%d",&lebar);
+luas = panjang * lebar;
+printf("jadi luas persegi panjang adalah %d\n",luas);
+
+float jari_jari, luaslingkaran, phi;
+phi = 3.14;
+printf("masukan jari-jari lingkaran = ");
+scanf("%f",&jari_jari);
+luaslingkaran = phi * jari_jari * jari_jari;
+printf("luas lingkaran adalah %f\n",luaslingkaran);
+
+// luas segitiga pr
+ 
+// //   double skorpemain = 95;
+// //   skorpemain = 96.5;
    
-//   double skorpemain = 95;
-//   skorpemain = 96.5;
+// //   printf("nilai pemain adalah %0.2f\n",skorpemain);
+// //   printf("==================================");
+// //   //skorpemain= variabel
+// //   //int itu type datanya
+// //   //95 itu nialinya
+// //   char ch = 'a';
+// //   ch = '1';
    
-//   printf("nilai pemain adalah %0.2f\n",skorpemain);
-//   printf("==================================");
-//   //skorpemain= variabel
-//   //int itu type datanya
-//   //95 itu nialinya
-//   char ch = 'a';
-//   ch = '1';
-   
-    // char d;
-    // d = 'p';
-    // printf("nilai d = %c\n",d);
-    // int a;
-    // printf("nilai a = %d\n",a);
-    // a = 20;
-    // float tampung;
-    // tampung = 10.5;
-    // // printf("nilai tampung adalah = %0.2f\n",tampung);
-    // int a;
-    // double e;
-    // e = 16.5;
-    // printf("nilai e adalah %0.2f\n",e);
-    // char d;
-    // d = 'a';
-    // printf("nilai d adalah berikut %c\n",d);
-    // int b = 25;
-    // printf("nilai a setelah insialisasi : %d\n",a);
-    // printf("nilai b = %d\n",b);
-    // int c;
-    // c = a+b;
-    // printf("nilai a + b adalah = %d\n ",c );
-    // Write C code here
-    //ini program untuk mencetak nama 
-    //ini saya buat tanggal 14 oktober
-    //hanya terdiri dari 1 file
-   //printf("Halo Nama Saya Ryan Mahasiswa STTPATI");
-    /* ini program pertama saya dalam menggunakan bahasa c
-    ksahfdkjsahfakjshfaks
-    saya mencoba belajar bahasa c dengan rajin dan faham */
+//     // char d;
+//     // d = 'p';
+//     // printf("nilai d = %c\n",d);
+//     // int a;
+//     // printf("nilai a = %d\n",a);
+//     // a = 20;
+//     // float tampung;
+//     // tampung = 10.5;
+//     // // printf("nilai tampung adalah = %0.2f\n",tampung);
+//     // int a;
+//     // double e;
+//     // e = 16.5;
+//     // printf("nilai e adalah %0.2f\n",e);
+//     // char d;
+//     // d = 'a';
+//     // printf("nilai d adalah berikut %c\n",d);
+//     // int b = 25;
+//     // printf("nilai a setelah insialisasi : %d\n",a);
+//     // printf("nilai b = %d\n",b);
+//     // int c;
+//     // c = a+b;
+//     // printf("nilai a + b adalah = %d\n ",c );
+//     // Write C code here
+//     //ini program untuk mencetak nama 
+//     //ini saya buat tanggal 14 oktober
+//     //hanya terdiri dari 1 file
+//   //printf("Halo Nama Saya Ryan Mahasiswa STTPATI");
+//     /* ini program pertama saya dalam menggunakan bahasa c
+//     ksahfdkjsahfakjshfaks
+//     saya mencoba belajar bahasa c dengan rajin dan faham */
     return 0;
 }
