c0d0e4adc9e5b420d0455cff1457c3a8fa3313ae
ryanyunus <ryandinus@gmail.com>
2023-10-14 03:29:48 +0700

    14 okt 2023 alpro

---
+++ alpro14oct2023.c
@@ -0,0 +1,49 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   
+   
+//   double skorpemain = 95;
+//   skorpemain = 96.5;
+   
+//   printf("nilai pemain adalah %0.2f\n",skorpemain);
+//   printf("==================================");
+//   //skorpemain= variabel
+//   //int itu type datanya
+//   //95 itu nialinya
+//   char ch = 'a';
+//   ch = '1';
+   
+    // char d;
+    // d = 'p';
+    // printf("nilai d = %c\n",d);
+    // int a;
+    // printf("nilai a = %d\n",a);
+    // a = 20;
+    // float tampung;
+    // tampung = 10.5;
+    // // printf("nilai tampung adalah = %0.2f\n",tampung);
+    // int a;
+    // double e;
+    // e = 16.5;
+    // printf("nilai e adalah %0.2f\n",e);
+    // char d;
+    // d = 'a';
+    // printf("nilai d adalah berikut %c\n",d);
+    // int b = 25;
+    // printf("nilai a setelah insialisasi : %d\n",a);
+    // printf("nilai b = %d\n",b);
+    // int c;
+    // c = a+b;
+    // printf("nilai a + b adalah = %d\n ",c );
+    // Write C code here
+    //ini program untuk mencetak nama 
+    //ini saya buat tanggal 14 oktober
+    //hanya terdiri dari 1 file
+   //printf("Halo Nama Saya Ryan Mahasiswa STTPATI");
+    /* ini program pertama saya dalam menggunakan bahasa c
+    ksahfdkjsahfakjshfaks
+    saya mencoba belajar bahasa c dengan rajin dan faham */
+    return 0;
+}
